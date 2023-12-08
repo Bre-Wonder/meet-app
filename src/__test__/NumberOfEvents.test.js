128fe736694fbad2be8b10df8d5817dda12e852c
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-12-07 17:48:12 -1000

    created NumberOfEvents component

---
+++ src/__test__/NumberOfEvents.test.js
@@ -0,0 +1,38 @@
+import NumberOfEvents from '../components/NumberOfEvents';
+import { fireEvent, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('<NumberOfEvents /> component', () => {
+
+  let eventCount
+
+  beforeEach(() => {
+    eventCount = render(<NumberOfEvents />);
+  });
+
+  //NumberOfEvents component contains role of textbox
+  test('NumberOfEvents component contains role of textbox', () => {
+    const textBoxElement = eventCount.getByRole('textbox');
+    expect(textBoxElement).toBeInTheDocument();
+  });
+
+  //Default value of the input field is 32
+  test('Default value of the input field is 32', () => {
+    const textBoxElement = eventCount.getByRole('textbox');
+    expect(textBoxElement).toHaveValue('32');
+
+  });
+
+  //Ensure that the value fo the NumberOfEvents component's textbox has a value that changes accordingly when a user types in it
+  test('Textbox changes accordingly to what user types in it', async () => {
+    const textBoxElement = eventCount.getByRole('textbox');
+    const inputValue = ''; // need to figure out how to dymically define 'inputValue'
+
+    await userEvent.type(textBoxElement, { target: { value: inputValue } });
+
+    expect(textBoxElement).toHaveValue('inputValue');
+
+  });
+
+
+});
