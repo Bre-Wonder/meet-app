4e4d61b077bbdcfe1d8553a633f3d27f2b363b2e
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2023-12-17 12:43:27 -1000

    added Event functions

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,27 @@
-const Event = () => {
-  return (
-    <li></li>
-  );
-}
+import { useState } from "react";
+import { getEvents } from "../api";
+import mockData from "../mock-data";
+
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  const startTime = event.created;
+
+ return (
+  <div>
+    <div className="eventName">{event.summary}</div>
+    <div className="eventStartTime">{startTime}</div>
+    <div className="eventLocations">{event.location}</div>
+
+    {showDetails ? (
+      <div>
+        <p>Event Details</p>
+        <button onClick={() => setShowDetails(false)}>Hide Details</button>
+      </div>
+    ) : (
+      <button onClick={() => setShowDetails(true)}>Show Details</button>
+    )}
+  </div>
+ );
+};
 
 export default Event;
