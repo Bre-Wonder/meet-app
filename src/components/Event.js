03ea596f773196945670fa52ea88191d513d3230
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-01-06 20:08:15 -0800

    changed to pass event prop

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,41 +1,25 @@
-import { render } from "@testing-library/react";
-import { useEffect, useState } from "react";
-import { getEvents } from "../api";
+import { useState } from "react";
 
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
-  const [allEvents, setAllEvents] = useState([]);
-  
-  useEffect(() => {
-    async function fetchEvents() {
-      try {
-        const events = await getEvents();
-        setAllEvents(events);
-      } catch (error) {
-        console.error("Error in fetching events", error);
-      }
-    }
 
-  fetchEvents();
-  }, []);
-  
-  const startTime = event.created;
+  let starTime = event.created;
 
- return (
-  <div>
-    <div className="eventName">{event.summary}</div>
-    <div className="eventStartTime">{startTime}</div>
-    <div className="eventLocations">{event.location}</div>
+  return (
+    <div>
+      <div className="eventName">{event.summary}</div>
+      <div className="eventStartTime">{starTime}</div>
+      <div className="eventLocations">{event.location}</div>   
 
-    {showDetails ? (
-      <div>
-        <p>Event Details</p>
-        <button onClick={() => setShowDetails(false)}>Hide Details</button>
-      </div>
-    ) : (
-      <button onClick={() => setShowDetails(true)}>Show Details</button>
-    )}
-  </div>
+      {showDetails ? (
+        <div>
+          <p>Event Details</p>
+          <button onClick={() => setShowDetails(false)}>Hide Details</button>
+        </div>
+      ) : (
+        <button onClick={() => setShowDetails(true)}>Show Details</button>
+      )}
+    </div>
  );
 };
 
