8a89cfea1f6ed27859903f6969a954cfe8a564e9
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-01-15 16:20:16 -0800

    added integration test and import from react

--- src/__test__/EventList.test.js
+++ src/__test__/EventList.test.js
@@ -1,7 +1,10 @@
-import { render } from '@testing-library/react';
+import { render, waitFor, within } from '@testing-library/react';
 import { getEvents } from '../api';
 import EventList from '../components/EventList';
+import App from '../App';
 
+
+//unit testing
 
 describe('<EventList /> component', () => {
   let EventListComponent;
@@ -20,4 +23,21 @@ describe('<EventList /> component', () => {
     EventListComponent.rerender(<EventList events={allEvents}/>);
     expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
   });
+});
+
+//start of integration testing
+
+describe('<EventList /> integration', () => {
+
+  //test #4
+  test('renders a list of 32 evetns when the app is mounted and rendered', async () => {
+    const AppComponent = render(<App />); 
+    const AppDOM = AppComponent.container.firstChild;
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      expect(EventListItems.length).toBeGreaterThan(0);
+    });
+  });
+
 });
