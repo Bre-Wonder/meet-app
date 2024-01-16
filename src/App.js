cab2d7b87d50d6f58f8b7a5477a6f34a22db59e8
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-01-15 16:19:24 -0800

    added state, useEffect and fetchData function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
-import './App.css';
 import NumberOfEvents from './components/NumberOfEvents';
+import { useEffect, useState } from 'react';
+import { extractLocations, getEvents } from './api';
+
+import './App.css';
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(33);
+  const [allLocations, setAllLocations] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
+
+
+
   return (
     <div className="App">
-      <CitySearch />
-      <EventList />   
-      <NumberOfEvents />  
+      <CitySearch allLocations={allLocations} />
+      <NumberOfEvents /> 
+      <EventList events={events} />   
     </div>
   );
 }
