b79366a4e99ff6057f109a8cac8c89a7b0de1840
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-01-17 20:44:52 -0800

    added integration test

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,6 +1,10 @@
-import { render } from '@testing-library/react';
+import { render, within } from '@testing-library/react';
+import userEvent  from '@testing-library/user-event';
+import { getEvents } from '../api';
 import App from '../App';
 
+
+// unit testing
 describe('<App /> component', () => {
   let AppDOM;
   beforeEach(() => {
@@ -11,13 +15,47 @@ describe('<App /> component', () => {
     expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
   });
 
-  // test #4 - CitySearch Test, when user begins to type in a city
+  // test #2 - CitySearch Test, when user begins to type in a city
   test('render CitySearch', () => {
     expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
   });
 
+  // test #3
   test('NumberOfEvents component is rendered', () => {
     expect(AppDOM.querySelector('#event-Count-Input')).toBeInTheDocument();
+  });
+
+});
+
+//integration testing
+describe('<App/> integraption', () => {
+
+  //test #4 
+  test('renders a list of evetns matching the city selected by the user', async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const CitySearchDOM = AppDOM.querySelector('#city-search');
+    const CitySearchInput = within(CitySearchDOM).queryByRole('textbox');
+
+    await user.type(CitySearchInput, "Berlin");
+    const berlinSuggestionItem = within(CitySearchDOM).queryByText('Berlin, Germany');
+    await user.click(berlinSuggestionItem);
+
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem')
+    
+    const allEvents = await getEvents();
+    const berlinEvents = allEvents.filter(
+      event => event.location === 'Berlin, Germany'
+    );
+    
+    expect(allRenderedEventItems.length).toBe(berlinEvents.length);
+
+    allRenderedEventItems.forEach(event => {
+      expect(event.textContent).toContain('Berlin, Germany');
+    });
   });
 
 });
