53cfff0a364045cafc03f4bb6600ddd79f86feb1
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-01-20 16:16:10 -0800

    added integration test for number of events

--- src/__test__/NumberOfEvents.test.js
+++ src/__test__/NumberOfEvents.test.js
@@ -2,6 +2,10 @@ import App from '../App';
 import NumberOfEvents from '../components/NumberOfEvents';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { toHaveDescription } from '@testing-library/jest-dom/dist/matchers';
+import { getEvents } from '../api';
+
+// unit testing
 
 describe('<NumberOfEvents /> component', () => {
 
@@ -35,5 +39,59 @@ describe('<NumberOfEvents /> component', () => {
 
   });
 
+});
+
+// integration testing
+
+describe('<NumberOfEvents /> integration', () => {
+
+  // test #5 testing that the user can change the number of events displayed
+
+  test('update the number of events with what the user types a new number', async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const noeDOM = AppDOM.querySelector('#event-Count-Input'); 
+    const noeTextBox = within(noeDOM).queryByRole('textbox');
+    // await user.click(noeTextBox); // not sure if I need this yet
+
+    await user.type(noeTextBox, '{backspace}{backspace}10');
+
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');
+
+    expect(allRenderedEventItems.length).toHaveValue('10');
+
+  });
 
 });
+
+//test from CitySearch test - to be deleted
+
+// test('renders a list of evetns matching the city selected by the user', async () => {
+//   const user = userEvent.setup();
+//   const AppComponent = render(<App />);
+//   const AppDOM = AppComponent.container.firstChild;
+
+//   const CitySearchDOM = AppDOM.querySelector('#city-search');
+//   const CitySearchInput = within(CitySearchDOM).queryByRole('textbox');
+
+//   await user.type(CitySearchInput, "Berlin");
+//   const berlinSuggestionItem = within(CitySearchDOM).queryByText('Berlin, Germany');
+//   await user.click(berlinSuggestionItem);
+
+//   const EventListDOM = AppDOM.querySelector('#event-list');
+//   const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem')
+  
+//   const allEvents = await getEvents();
+//   const berlinEvents = allEvents.filter(
+//     event => event.location === 'Berlin, Germany'
+//   );
+  
+//   expect(allRenderedEventItems.length).toBe(berlinEvents.length);
+
+//   allRenderedEventItems.forEach(event => {
+//     expect(event.textContent).toContain('Berlin, Germany');
+//   });
+// });
