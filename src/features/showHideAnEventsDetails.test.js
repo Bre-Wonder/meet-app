7910d1922fd6b526924affcfcb9cc810b6075ad8
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-02-03 20:02:50 -0800

    adding test details

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,22 +1,36 @@
+import { render, waitFor, within } from "@testing-library/react";
 import { loadFeature, defineFeature } from "jest-cucumber";
+import App from "../App";
+import Event from "../components/Event";
 
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
 
 defineFeature(feature, test => {
 
-
   test('An event element is collapsed by default', ({ given, when, then }) => {
 
+   let AppComponent 
     given('the app is open', () => {
+      AppComponent = render(<App />);
 
     });
 
-    when('the app lists out events happening in different cities', () => {
+    when('the app lists out events happening in different cities', async () => {
+      const AppDOM = AppComponent.container.firstChild;
+        const EventListDOM = AppDOM.querySelector('#event-list');
+        await waitFor(() => {
+          const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');
+          expect(allRenderedEventItems.length).toBe(33);
+        });
 
     });
 
     then('an events details should be hidden by default', () => {
+      const AppDOM = AppComponent.container.firstChild;
+      const EventDOM = AppDOM.querySelector('#event');
+      expect(EventDOM.container.querySelector('.HideDetails')).not.toBeInTheDocument();
+
 
     });
   });
