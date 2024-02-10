e7d096af712f9c66521fb43c5ed4e6f6e5cfa543
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-02-10 13:34:46 -0800

    passed dummy prop for test to pass

--- src/__test__/CitySearch.test.js
+++ src/__test__/CitySearch.test.js
@@ -11,7 +11,7 @@ import { extractLocations, getEvents } from '../api';
 describe('<CitySearch /> component', () => {
   let CitySearchComponent;
   beforeEach(() => {
-    CitySearchComponent = render(<CitySearch allLocations={[]}/>);
+    CitySearchComponent = render(<CitySearch allLocations={[]} setInfoAlert={() => {}}/>);
   });
 
   //test #1
@@ -43,7 +43,7 @@ describe('<CitySearch /> component', () => {
     const user = userEvent.setup();
     const allEvents = await getEvents();
     const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} setInfoAlert={() => {}} />);
 
     // if user types 'Berlin' to find events there
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -68,6 +68,7 @@ describe('<CitySearch /> component', () => {
     CitySearchComponent.rerender(<CitySearch 
       allLocations={allLocations}
       setCurrentCity={() => {}}
+      setInfoAlert={() => {}}
       />);
 
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
