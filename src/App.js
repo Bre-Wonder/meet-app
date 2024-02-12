f275350a3db9a1189e6e55e36a1dc0cc7fc75385
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-02-11 16:10:11 -0800

    added WarningAlert Button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
-import { InfoAlert, ErrorAlert } from './components/Alert';
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
 import './App.css';
 
@@ -14,8 +14,14 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   useEffect(() => {
+    if (navigator.onLine) {
+      setWarningAlert("");
+    } else {
+      setWarningAlert("Please be aware, your application is now offline. Listed events may not be up to date.")
+    }
     fetchData();
   }, [currentCity, currentNOE]);
 
@@ -41,6 +47,7 @@ const App = () => {
       <div className='alerts-container'>
         {infoAlert.length ? <InfoAlert text={ infoAlert } /> : null}
         {errorAlert.length ? <ErrorAlert text={ errorAlert } /> : null}
+        {warningAlert.length ? <WarningAlert text={ warningAlert } /> : null}
       </div>
       <CitySearch allLocations={allLocations} 
         setCurrentCity={setCurrentCity}
