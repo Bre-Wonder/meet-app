75dde7068ef149c4f4a5b2eab6a7a6bc81427163
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-02-14 18:40:16 -0800

    created new component for chart

---
+++ src/components/CityEventsChart.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+
+const CityEventChart = ({ allLocations, events }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(getData());
+  }, [`${events}`]);
+
+  const getData = () => {
+    const data = allLocations.map((location) => {
+      const count = events.filter((event) => event.location === location).length
+      const city = location.split(', ')[0]
+      return {city, count};
+    })
+    return data;
+  };
+}
+
+export default CityEventChart;
