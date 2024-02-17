6e630dd59ef6e0a42d579bbd6a3e041c07cc7754
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-02-17 15:26:21 -0800

    updated margin for chart on left

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -32,7 +32,7 @@ const CityEventsChart = ({ allLocations, events }) => {
           top: 20,
           right: 20,
           bottom: 60,
-          left: 20,
+          left: -30,
         }}
       >
         <CartesianGrid />
