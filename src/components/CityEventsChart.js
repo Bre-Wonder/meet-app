aa46f1c02fd90fa12abff3116883f8cfa8329c67
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-02-17 14:36:18 -0800

    fixed hook dependancy error

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -1,18 +1,26 @@
-import { useState, useEffect } from "react";
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+import { useState, useEffect } from 'react';
+import {
+  ScatterChart,
+  Scatter,
+  XAxis, YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer
+} from 'recharts';
 
 const CityEventsChart = ({ allLocations, events }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     setData(getData());
-  }, [`${data}`]);
+    // eslint-disable-next-line
+  }, [`${events}`]);
 
   const getData = () => {
     const data = allLocations.map((location) => {
       const count = events.filter((event) => event.location === location).length
       const city = location.split(', ')[0]
-      return {city, count};
+      return { city, count }; // count, number + short hand for { city: city, count: count}
     })
     return data;
   };
@@ -25,18 +33,16 @@ const CityEventsChart = ({ allLocations, events }) => {
           right: 20,
           bottom: 20,
           left: 20,
-        }}>
-
+        }}
+      >
         <CartesianGrid />
-        <XAxis type="catagory" dataKey="city" name="City" />
-        <YAxis type="number" dataKey="count" name="Number of Events" />
+        <XAxis type="catagory" dataKey="city" name="City"/>
+        <YAxis type="number" dataKey="count" name="Number of Events"/>
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter name="A school" data={data} fill="#8884d8" />
       </ScatterChart>
     </ResponsiveContainer>
   );
-
-
 }
 
 export default CityEventsChart;
